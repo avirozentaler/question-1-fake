422432155338df5c8da20967812bbae9a1dc6b70
avirozentaler <46267593+avirozentaler@users.noreply.github.com>
2019-01-03 23:44:00 +0200

    Add files via upload

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+                if (i%2!=0&&i%3!=0&&i%5!=0&&i%7!=0||i==3||i==5||i==7)
+                   
+                {
+                    Console.WriteLine(i);
+                }
+            {
+
+            }
+        }
+    }
+}
